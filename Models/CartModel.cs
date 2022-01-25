042bf98c726c2cbea1bcb2b9f396541d642e37b3
Gökhan SEVİM <mail@gokhansevim.com>
2022-01-26 02:09:44 +0300

    Initial commit

---
+++ Models/CartModel.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Core.Domain.Catalog;
+using Nop.Core.Domain.Orders;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.DiscountRules.HasCategory.Models
+{
+    public record CartModel
+    {
+        public ShoppingCartItem CartItem { get; set; }
+
+        public IList<ProductCategory> Category { get; set; }
+    }
+}
